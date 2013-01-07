60c204a0b618374105058975afdfbd6c35595f02
Dennis Ruffer <daruffer@gmail.com>
2013-01-06 20:37:50 -0800

    This weekend's work, bringing in vmgen documentation
    Now there appears to be a problem exporting pdf output in Linux

--- afvm/mini.h
+++ afvm/mini.h
@@ -62,10 +62,10 @@ void vm_print_profile(FILE *file);
 #define vm_a2Cell(x,_cell)      ((_cell)=(Cell)(x))
 #define vm_Cell2Cell(_x,_y)     ((_y)=(Cell)(_x))
 /* the cast in vm_Cell2Cell is needed because the base type for
-   inst-stream is Cell, but *IP is an Inst */
+   inst-stream is Cell, but *IP is an Inst */
 
 /* for future extensions */
-#define IMM_ARG(access,value) (access)
+#define IMM_ARG(access,value) (access)
 
 #define VM_IS_INST(inst, n) ((inst) == vm_prim[n])
 
